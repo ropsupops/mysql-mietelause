54bfcf1a995e1b3f571936f26eb99c5ce2857d47
ropsupops <99011693+ropsupops@users.noreply.github.com>
2022-03-08 00:31:03 +0200

    Pieniä muutoksia

--- mysql-mietelause.php
+++ mysql-mietelause.php
@@ -1,26 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
- <title>MySQL-mietelause</title>
+  <title>MySQL-mietelause</title>
 </head>
 <body>
  
- <?php
-$tietokantayhteys = 
-  mysqli_connect("", "kurssi", "kood10tus", "php2022");
+<?php
+  $tietokantayhteys = mysqli_connect("", "kurssi", "kood10tus", "php2022");
 
-if (mysqli_connect_errno()) {
-  echo "Yhteysvirhe tietokantaan: " . mysqli_connect_error();
-}
+  if (mysqli_connect_errno()) {
+    echo "Yhteysvirhe tietokantaan: " . mysqli_connect_error();
+  }
 
-mysqli_set_charset($tietokantayhteys,"utf8-fi");
+  mysqli_set_charset($tietokantayhteys,"utf8-fi");
 
-$sql = "SELECT * FROM roope_mietelauseet";
-$result = mysqli_query($tietokantayhteys, $sql);
+  $sql = "SELECT * FROM roope_mietelauseet";
+  $result = mysqli_query($tietokantayhteys, $sql);
 
-mysqli_query($tietokantayhteys, $sql)
-  or die("Virhe: " . mysqli_error($tietokantayhteys));
+  mysqli_query($tietokantayhteys, $sql)
+    or die("Virhe: " . mysqli_error($tietokantayhteys));
 ?>
- 
+
+
+
 </body>
 </html>
