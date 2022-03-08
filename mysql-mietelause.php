c9d86772fb1f782a61c14676980d04b047b100d4
ropsupops <99011693+ropsupops@users.noreply.github.com>
2022-03-08 19:49:42 +0200

    Muokataan select lausetta ja lisätään css-osuuden alku

--- mysql-mietelause.php
+++ mysql-mietelause.php
@@ -1,10 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>MySQL-mietelause</title>
-</head>
-<body>
- 
 <?php
   $tietokantayhteys = mysqli_connect("", "kurssi", "kood10tus", "php2022");
 
@@ -14,12 +7,31 @@
 
   mysqli_set_charset($tietokantayhteys,"utf8-fi");
 
-  $sql = "SELECT * FROM roope_mietelauseet";
+  $sql = "SELECT * OR BY RAND() FROM roope_mietelauseet";
   $result = mysqli_query($tietokantayhteys, $sql);
 
   mysqli_query($tietokantayhteys, $sql)
     or die("Virhe: " . mysqli_error($tietokantayhteys));
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>MySQL-mietelause</title>
+  <style>
+    body{
+      background-color:
+      <?php
+        if ($row[0] == "Idän filosofit") {
+          echo "green";
+        } else {
+          echo "blue";
+        }
+      ?>;
+    }
+  </style>
+</head>
+<body>
 
 
 
